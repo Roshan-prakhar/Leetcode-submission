6085ac36af6f8f39c0909de4657b79fb7e3da617
Roshan Prakhar <rtgrosh@gmail.com>
2026-08-25 23:42:15 +0530

    Time: 49 ms (6.56%) | Memory: 59.9 MB (22.26%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+ bool solve(long long n,vector<int>&bat,long long mid)
+ {
+    long long cnt=0;
+long long devices=n;
+    long long j=bat.size()-1;
+    while(j>=0)
+    {
+        if(bat[j]>=mid)
+        {
+            cnt++;
+            devices--;
+            j--;
+        }
+        else
+        {
+            if(devices==0) break;
+            long long sum=0;
+            for(long long idx=j;idx>=0;--idx) sum+=bat[idx];
+            long long avg=sum/devices;
+            if(avg>=(long long)mid)
+            {
+                cnt+=devices;
+                break;
+            }
+            else{
+                break;
+            }
+        }
+
+    }
+
+    return cnt>=n;
+ }
+    long long maxRunTime(int n, vector<int>& bat) {
+        sort(bat.begin(),bat.end());
+        long long ts=0;
+        for(long long i : bat) ts+=i;
+        long long ans=0;
+        long long low=0;
+        long long high=ts;
+        while(low<=high)
+        {
+            long long mid=(low+high)>>1;
+            if(solve(n,bat,mid))
+            {
+                  ans=mid;
+                 low=mid+1;
+            }
+            else high=mid-1;
+        }
+
+        return ans;
+
+
+        
+    }
+};
