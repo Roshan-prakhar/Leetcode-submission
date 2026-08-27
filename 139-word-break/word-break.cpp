6b2ab60428f946a9d705e269d7d625d59c950449
Roshan Prakhar <rtgrosh@gmail.com>
2026-08-27 13:14:31 +0530

    Time: 19 ms (14.34%) | Memory: 17.9 MB (17.19%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    bool solve(string s,int idx,set<string>&st,vector<int>&dp)
-    {
-         if(idx>=s.size()) return 1;
-        if(dp[idx]!=-1) return dp[idx];
-         string temp;
-         for(int i=idx;i<s.size();++i)
-         {
-            temp.push_back(s[i]);
-            if(st.count(temp)!=0)
-            if(solve(s,i+1,st,dp)) return dp[idx]=true;
-         }
-         return dp[idx]=false;
-    }
     bool wordBreak(string s, vector<string>& wd) {
         int sze=s.size();
-       set<string>st;
-       for(string x : wd) st.insert(x);
-       vector<int>dp(sze+1,-1);
-       return solve(s,0,st,dp);
+        set<string>st;
+        for(int i=0;i<wd.size();++i) st.insert(wd[i]);
+        // leetcode
+        // DP[i] -> means starting from (0.....i-1) could be matched or not
+        // ans = DP[n]==true;
+        vector<int>dp(sze+1,0);
+        dp[0]=1;
+        for(int i=1;i<=sze;++i)
+        {
+            for(int j=0;j<i;++j)
+            {
+                string temp=s.substr(j,i-j);
+                if(dp[j]==1 && st.count(temp)!=0)
+                {
+                    dp[i]=true;
+                }
+            }
+        }
 
+        return dp[sze];
 
-
-        
+                
     }
 };
